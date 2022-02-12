89c96fae6a8a1acb16330bd34a238bcfe07b5c8f
edanc <edanc@nsogroup.com>
2022-02-12 14:14:05 +0200

    in the process of moving everything to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,66 @@
-const minMaxNormalization = (
-  dataSet: Array<number>,
-  scale: [number, number] = [0, 1],
-  forceMinMaxValues?: [number?, number?]
-) => {
-  const maxValue = forceMinMaxValues?.[1] ?? Math.max(...dataSet)
-  const minValue = forceMinMaxValues?.[0] ?? Math.min(...dataSet)
+const isFunction = (v: any): v is Function => typeof v === "function"
+const isString = (v: any): v is string => typeof v === "string"
+const isNumberArray = (v: any[]): v is number[] => typeof v[0] === "number"
+const isObject = (v: any): v is object =>
+  v && typeof v === "object" && v.constructor === Object
 
-  const diff = maxValue - minValue
-  if (diff === 0) return dataSet.map(_ => scale[1])
+const run = <T = any>(func: ((...args: any[]) => T) | T, ...args: any[]): T =>
+  isFunction(func) ? func(...args) : func
 
-  const normalizeDatum = (datum: number) =>
-    scale[0] + ((datum - minValue) * (scale[1] - scale[0])) / diff
-
-  return dataSet.map(normalizeDatum)
+type Keys = {
+  byValue: string | ((o: object) => string)
+  newKey: string
 }
 
-export default minMaxNormalization
+type Scales = {
+  scale?: [number, number]
+  forceMinMaxValues?: [number?, number?]
+}
+
+type NumberSet = (dataSet: number[], options: Scales) => number[]
+type ObjectSet = (dataSet: object[], options: Scales & Keys) => object[]
+
+interface Func1 {
+  (dataSet: number[], options: Scales): number[]
+  (dataSet: object[], options: Scales & Keys): object[]
+}
+
+type Func2 = NumberSet | ObjectSet
+
+type Func3<T extends number[] | object[]> = T extends number[]
+  ? NumberSet
+  : ObjectSet
+
+const dataNormalize: Func3<number[] | object[]> = (dataSet, options) => {
+  // if (!isNumberArray(dataSet) && (!options.byValue || !options.newKey)) {
+  //   console.error("Insufficient arguments have passed")
+  //   return dataSet
+  // }
+
+  const { scale = [0, 1], forceMinMaxValues } = options || {}
+
+  const getValue = (o: { [index: string]: any }) => {
+    const { byValue } = options
+    return isString(byValue) ? o[byValue] : byValue?.(o)
+  }
+
+  const values = isNumberArray(dataSet) ? dataSet : dataSet.map(getValue)
+
+  const maxValue = forceMinMaxValues?.[1] ?? Math.max(...values)
+  const minValue = forceMinMaxValues?.[0] ?? Math.min(...values)
+
+  const diff = maxValue - minValue
+
+  const normalizeDatum = (datum: number) => {
+    if (diff === 0) return scale[1]
+    return scale[0] + ((datum - minValue) * (scale[1] - scale[0])) / diff
+  }
+
+  if (isNumberArray(dataSet)) return dataSet.map(normalizeDatum)
+
+  return dataSet.forEach((o: { [index: string]: any }) => {
+    o[options.newKey] = normalizeDatum(getValue(o))
+  })
+}
+
+export default dataNormalize
--- src/test.ts
+++ src/test.ts
@@ -1,18 +1,49 @@
-import minMaxNormalization from "./index"
+import dataNormalize from "./index"
 
-test("minMaxNormalization", () => {
-  expect(minMaxNormalization([10], [3, 6], [10, 70])).toStrictEqual([3])
-  expect(minMaxNormalization([20], [3, 6], [10, 70])).toStrictEqual([3.5])
-  expect(minMaxNormalization([70], [3, 6], [10, 70])).toStrictEqual([6])
+describe("dataNormalize", () => {
+  // it("plain array of numbers", function () {
+  //   expect(
+  //     dataNormalize([10], { scale: [3, 6], forceMinMaxValues: [10, 70] })
+  //   ).toStrictEqual([3])
+  //   expect(
+  //     dataNormalize([20], { scale: [3, 6], forceMinMaxValues: [10, 70] })
+  //   ).toStrictEqual([3.5])
+  //   expect(
+  //     dataNormalize([70], { scale: [3, 6], forceMinMaxValues: [10, 70] })
+  //   ).toStrictEqual([6])
+  //
+  //   expect(dataNormalize([1, 2, 3, 4, 5])).toStrictEqual([
+  //     0, 0.25, 0.5, 0.75, 1
+  //   ])
+  //   expect(
+  //     dataNormalize([6, 12], { scale: [0, 1], forceMinMaxValues: [0] })
+  //   ).toStrictEqual([0.5, 1])
+  //   expect(dataNormalize([1, 1, 1, 1, 1])).toStrictEqual([1, 1, 1, 1, 1])
+  //   expect(dataNormalize([0, ...Array(50).fill(1), 50])).toStrictEqual([
+  //     0,
+  //     ...Array(50).fill(0.02),
+  //     1
+  //   ])
+  // })
 
-  expect(minMaxNormalization([1, 2, 3, 4, 5])).toStrictEqual([
-    0, 0.25, 0.5, 0.75, 1
-  ])
-  expect(minMaxNormalization([6, 12], [0, 1], [0])).toStrictEqual([0.5, 1])
-  expect(minMaxNormalization([1, 1, 1, 1, 1])).toStrictEqual([1, 1, 1, 1, 1])
-  expect(minMaxNormalization([0, ...Array(50).fill(1), 50])).toStrictEqual([
-    0,
-    ...Array(50).fill(0.02),
-    1
-  ])
+  it("array of objects", function () {
+    const dataSet1 = [
+      { value: 1, name: "a" },
+      { value: 2, name: "b" },
+      { value: 3, name: "c" },
+      { value: 4, name: "d" },
+      { value: 5, name: "e" }
+    ]
+    expect(
+      dataNormalize(dataSet1, { byValue: "value", newKey: "newKey" })
+    ).toStrictEqual([0, 0.25, 0.5, 0.75, 1])
+    const dataSet2 = [
+      { a: { value: 1, name: "a" } },
+      { b: { value: 2, name: "b" } },
+      { c: { value: 3, name: "c" } },
+      { d: { value: 4, name: "d" } },
+      { e: { value: 5, name: "e" } }
+    ]
+    // expect(dataNormalize(dataSet2)).toStrictEqual([0, 0.25, 0.5, 0.75, 1])
+  })
 })
